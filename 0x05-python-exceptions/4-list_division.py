8f658c237950d93304d1e5da1f105b2fd09b1e17
Jamescog <jamescog72@gmail.com>
2022-07-18 13:31:12 -0700

    append is fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,12 +4,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(0, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-            new_list.append(result)
-        except TypeError:
+        except (ValueError, TypeError):
             print("wrong type")
+            result  = 0
         except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
-            return new_list
+            new_list.append(result)
+    return new_list
