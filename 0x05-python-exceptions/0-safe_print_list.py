03a8fa60c2de43f9c08faff1cd25b4064b70afe1
Jamescog <jamescog72@gmail.com>
2022-07-18 03:44:29 -0700

    type casting is implemented

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         result = my_list[:x]
-        return result
+        result_holder =  ''.join(str(e) for e in result)
+        return int(result_holder)
     except IndexError:
-        return my_list
+        result_holder = ''.join(str(e) for e in my_list)
+        return int(result_holder)
