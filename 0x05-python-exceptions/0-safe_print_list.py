4fbb43d829d4b55801f859cb66bfbed72431ab19
Jamescog <jamescog72@gmail.com>
2022-07-18 02:54:56 -0700

    type of exception is specified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        result = mylist[:x]
+        result = my_list[:x]
         return result
-    except Indexerror:
-        for i in mylist:
+    except IndexError:
+        for i in my_list:
             element = i
         return element
