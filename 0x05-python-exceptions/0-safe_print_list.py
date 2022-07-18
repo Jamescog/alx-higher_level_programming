b8c366e7d7710705538221d2e78c287c80433671
Jamescog <jamescog72@gmail.com>
2022-07-18 02:47:18 -0700

    excepting handiling 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        result = mylist[:x]
+        return result
+    except Indexerror:
+        for i in mylist:
+            element = i
+        return element
