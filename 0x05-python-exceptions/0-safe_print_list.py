e210cf3affb8a1db6934bade59041ee7dfa9135c
Jamescog <jamescog72@gmail.com>
2022-07-18 03:21:07 -0700

    exception is handled

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,4 @@ def safe_print_list(my_list=[], x=0):
         result = my_list[:x]
         return result
     except IndexError:
-        for i in my_list:
-            element = i
-        return element
+        return my_list
