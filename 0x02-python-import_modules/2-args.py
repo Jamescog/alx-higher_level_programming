f6777a046eab5b63f046f5fea440b7aae59ab825
Jamescog <jamescog72@gmail.com>
2022-08-04 04:40:55 -0700

    command line argument 101

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
 
-if __name == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    arg_num = len(argv)
-    i = 1
-    if arg_num == 0:
-        print("{:d} arguments.".format(arg_num))
-    elif arg_num == 1:
-        print("{:d} argument.".format(arg_num))
-        print("{:d}: {:s}".format(i, sys.argv[1]))
+    n = len(argv) - 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("{}: {}".format(n, argv[n+1]))
     else:
-        print("{:d} arguments.".format(arg_num))
-        while i < arg_num:
-            print("{:d}: {:s}".format(i, sys.argv[i]))
-            i += 1
+        print("{} arguments:".format(n))
+        for i in range(1, n):
+            print("{}: {}".format(i, argv[i]))
