63c9e436e3ab7f93c6b9d8ba14366b4f6aa16222
Jamescog <jamescog72@gmail.com>
2022-08-04 04:53:09 -0700

    add inifinity

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-    arg_num = len(argv) - 1
-    if arg_num == 0:
-        print("{}".format(arg_num))
-    else:
-        result = []
-        for i in range(1, arg_num + 1):
-            result.append(int(argv[i]))
-        print("{}".format(sum(result)))
+    from sys import argv
+
+    n = len(argv)
+    result = 0
+    for i in range(1, n):
+        result += int(argv[i])
+    print("{}".format(result))
