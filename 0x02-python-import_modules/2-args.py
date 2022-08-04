57d164f00e4d1a4f29dc10f23979cb7b7bbc5d3b
Jamescog <jamescog72@gmail.com>
2022-08-04 04:45:28 -0700

    namespace fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
 
     n = len(argv) - 1
@@ -7,8 +7,8 @@ if __name__ = "__main__":
         print("{} arguments.".format(n))
     elif n == 1:
         print("{} argument:".format(n))
-        print("{}: {}".format(n, argv[n+1]))
+        print("{}: {}".format(n, argv[n]))
     else:
         print("{} arguments:".format(n))
-        for i in range(1, n):
+        for i in range(1, n+1):
             print("{}: {}".format(i, argv[i]))
