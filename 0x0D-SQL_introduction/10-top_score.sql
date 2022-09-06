0ff3ea002d9ec9410efa8cd95a5bc880f0f2d01f
Jamescog <jamescog72@gmail.com>
2022-09-06 09:40:05 -0700

    order fixed

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,8 @@
 -- list all records of the table second_table of database
 -- start with top score on top
-SELECT name, score
-FROM second_table
-ORDER BY score DESC;
+SELECT
+	score, name
+FROM
+	second_table
+ORDER BY
+	score DESC;
