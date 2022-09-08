47fc915c9645996d8de76696dcc6f792f2fd78de
Jamescog <jamescog72@gmail.com>
2022-09-08 01:03:40 -0700

    always name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,6 @@
--- creates the table force_name on your MySQL server
--- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates the table force_name on my MySQL server
+-- if the table exists it won't fail
+CREATE TABLE 
+	IF NOT EXISTS 'force_name'
+		(id INT,
+		name VARCHAR(256));
