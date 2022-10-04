74678814acfadf71db38b20c3e608a885d16f6c8
Jamescog <jamescog72@gmail.com>
2022-10-04 02:49:41 -0700

    fixed condtion

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const myArr = process.argv.slice(2);
-const count = myArr.length;
-if (count === 0) {
+if (typeof myArr[0] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(myArr[0]);
