6f6f8c8e583b54cb1698ed9c9629a77c0a20f7d6
Jamescog <jamescog72@gmail.com>
2022-10-04 03:34:44 -0700

    prints two arguemnt passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-myArr = process.argv.slice(2);
-count = myArr.length;
-if 
+const myArr = process.argv.slice(2);
+const count = myArr.length;
+if (count === 0) {
+  console.log('undefined is undefined');
+} else if (count === 1) {
+  console.log(myArr[0] + ' is undefined');
+} else {
+  console.log(myArr[0] + ' is ' + myArr[1]);
+}
