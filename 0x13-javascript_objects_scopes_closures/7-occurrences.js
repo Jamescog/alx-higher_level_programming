f064b2d1edfe59b1d8723b97c470399821ebdf92
Jamescog <jamescog72@gmail.com>
2022-10-05 11:45:54 -0700

    count the occurance

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (target of list) {
+    if (target === searchElement) {
+      counter++;
+    }
+  }
+  return counter
+}
