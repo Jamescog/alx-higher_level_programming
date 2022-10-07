74f13a9c31bed374821e8eb59e04c37a83ce74f3
Yaekob Demisse <94002610+Jamescog@users.noreply.github.com>
2022-10-07 14:02:38 +0300

    show state table

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+"""
+LIST EVERY THING FROM DATABASE ON THE TABELE "states"
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_connection= MySQLdb.connect(user=argv[0], passwd=argv[1], db=argv[2], port=3306)
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM state")
+    result = cursor.fetchall()
+    for row in result:
+        print(i)
+    
