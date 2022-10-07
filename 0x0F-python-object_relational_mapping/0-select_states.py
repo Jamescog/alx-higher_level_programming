88ba35800b7faf783dc9d374f97396b1f3573ffb
Yaekob Demisse <94002610+Jamescog@users.noreply.github.com>
2022-10-07 14:04:47 +0300

    fix table name

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == "__main__":
     db_connection= MySQLdb.connect(user=argv[0], passwd=argv[1], db=argv[2], port=3306)
     cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM state")
+    cursor.execute("SELECT * FROM states")
     result = cursor.fetchall()
     for row in result:
         print(i)
