4a67b41a1cafcdbe1a864f2eac12e6adef358754
Jamescog <jamescog72@gmail.com>
2022-10-12 12:20:18 +0300

    where with join

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    this script make query based on user input
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cursor = db.cursor()
+    query = """
+                SELECT cities.id, cities.name, states.name
+                FROM cities JOIN states ON cities.state_id = states.id
+                WHERE states.name = '{}'
+            """.format(argv[4])
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row[1], end="")
