cbdc2d3828ba0371ab0839a2ea172c3aab1e5a49
Jamescog <jamescog72@gmail.com>
2022-10-12 14:52:35 +0300

    first come first served

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    prints the first entity that satisifies the query
+"""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).first()
+    if result:
+        print("{}: {}".format(result.id, result.name))
+    else:
+        print()
