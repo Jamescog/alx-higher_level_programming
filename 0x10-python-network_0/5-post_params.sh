41303a852cf4f56c749141032c4842fb32f467aa
Jamescog <jamescog72@gmail.com>
2022-10-20 16:35:00 +0300

    two post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to passed URL
-curl -s -d email=test@gmail.com -d subject=I will always be here for PLD "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
