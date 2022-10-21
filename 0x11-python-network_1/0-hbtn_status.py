da6183b8b33b122493cba5a1e9a889d9f844ceec
Jamescog <jamescog72@gmail.com>
2022-10-21 12:49:27 +0300

    with added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ The package used: urllib
 
 if __name__ == "__main__":
     from urllib import request
-
-    r = request.urlopen("https://alx-intranet.hbtn.io/status")
-    print("""Body response:
-        - type: {}
-        - content: {}
-        - utf8 content: {}""".format(type(r.peek()), r.peek(), r.msg))
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
+        print("""Body response:
+                     - type: {}
+                     - content: {}
+                     - utf8 content: {}""".format(type(r.read()),
+                                                  r.read(), r.msg))
