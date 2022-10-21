646f31d1d5b5d69811f1d2f499a015748348b840
Jamescog <jamescog72@gmail.com>
2022-10-21 14:21:17 +0300

    getting specific header using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, send request to the URL
+and displays the value of X-Request-Id in response header
+"""
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    respo = get(argv[1])
+    print(respo.headers['X-Request-Id'])
+    respo.close()
