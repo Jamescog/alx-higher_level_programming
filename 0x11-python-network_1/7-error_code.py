623b3c94eab44283d928a3bb0b7039d405108869
Jamescog <jamescog72@gmail.com>
2022-10-21 15:33:46 +0300

    status code reporter 2

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL
+and displays the body of the response
+
+if the HTTP status code is greater than or equal 400
+it will print Error code: followed by the value of the 
+HTTP statuse code
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from requests import get
+    r = get(argv[1])
+    if(r.ok):
+        print(r.content)
+    else:
+        print(f"Error code: {r.status_code}")
