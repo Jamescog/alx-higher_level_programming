308f141d85cebb140441d21d43121562a32e2560
Jamescog <jamescog72@gmail.com>
2022-10-21 15:37:05 +0300

    PEP8 fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@ This script takes in a URL, sends a request to the URL
 and displays the body of the response
 
 if the HTTP status code is greater than or equal 400
-it will print Error code: followed by the value of the 
+it will print Error code: followed by the value of the
 HTTP statuse code
 """
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     from sys import argv
     from requests import get
     r = get(argv[1])
-    if(r.ok):
+    if (r.ok):
         print(r.content)
     else:
         print(f"Error code: {r.status_code}")
