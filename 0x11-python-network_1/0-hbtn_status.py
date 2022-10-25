b15eaf90d8d3ad61f7c9ac558e3ef7c4d40bd469
Jamescog <jamescog72@gmail.com>
2022-10-25 13:05:25 +0300

    after first deadline correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,16 @@ This script fetchs  https://alx-intranet.hbtn.io/status
 The package used: urllib
 """
 
+from urllib import response
+
+
 if __name__ == "__main__":
     from urllib import request
     with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
-        print("""Body response:
-                     - type: {}
-                     - content: {}
-                     - utf8 content: {}""".format(type(r.read()),
-                                                  r.read(), r.msg))
+        binary = r.read()
+        ty = type(binary)
+        m = r.msg
+    print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(ty, binary, m))
